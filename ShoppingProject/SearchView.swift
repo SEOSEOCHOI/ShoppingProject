b453754c985fd55870ce383a02aa4dd92289e308
seogyeong1 <82697951+seogyeong1@users.noreply.github.com>
2024-05-09 20:57:47 +0900

    Initial Commit

---
+++ ShoppingProject/SearchView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShoppingProject
+//
+//  Created by 최서경 on 5/9/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShoppingProject/ShoppingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingProjectApp.swift
+//  ShoppingProject
+//
+//  Created by 최서경 on 5/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SearchView()
+        }
+    }
+}
